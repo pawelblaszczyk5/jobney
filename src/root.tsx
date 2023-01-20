63b0c7ed13bb4f2488e5f7e95aa9340baff54a79
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2023-01-20 19:12:52 +0100

    Tailwind initial setup

--- src/root.tsx
+++ src/root.tsx
@@ -2,6 +2,9 @@
 import { type JSX, Suspense } from 'solid-js';
 import { Body, ErrorBoundary, FileRoutes, Head, Html, Meta, Routes, Scripts, Title } from 'solid-start';
 
+// eslint-disable-next-line import/no-unassigned-import, import/extensions -- Tailwind stylesheet
+import '~/root.css';
+
 export default (): JSX.Element => (
 	<Html lang="en">
 		<Head>
@@ -9,7 +12,7 @@ export default (): JSX.Element => (
 			<Meta charset="utf-8" />
 			<Meta name="viewport" content="width=device-width, initial-scale=1" />
 		</Head>
-		<Body>
+		<Body class="p-4">
 			<Suspense>
 				<ErrorBoundary>
 					<Routes>
