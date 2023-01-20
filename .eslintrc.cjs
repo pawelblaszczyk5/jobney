5b7e0556aa4634af244b3683af427a7e2ffac24a
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2023-01-20 20:10:11 +0100

    Add solid specific rules to ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
 	ignorePatterns: ['scripts/**/*'],
-	plugins: ['import', 'prettier', 'tailwindcss'],
+	plugins: ['import', 'prettier', 'tailwindcss', 'solid'],
 	overrides: [
 		{
 			extends: [
@@ -57,6 +57,23 @@ module.exports = {
 				'tailwindcss/no-arbitrary-value': 'off',
 				'tailwindcss/no-custom-classname': 'error',
 				'tailwindcss/no-contradicting-classname': 'error',
+				// Solid.js
+				'solid/components-return-once': 'error',
+				'solid/event-handlers': 'error',
+				'solid/imports': 'error',
+				'solid/jsx-no-duplicate-props': 'error',
+				'solid/jsx-no-script-url': 'error',
+				'solid/jsx-no-undef': 'error',
+				'solid/jsx-uses-vars': 'error',
+				'solid/no-destructure': 'error',
+				'solid/no-proxy-apis': 'error',
+				'solid/no-react-deps': 'error',
+				'solid/no-react-specific-props': 'error',
+				'solid/no-unknown-namespaces': 'error',
+				'solid/prefer-for': 'error',
+				'solid/prefer-show': 'error',
+				'solid/self-closing-comp': 'error',
+				'solid/style-prop': 'error',
 				// builtin
 				'no-restricted-imports': [
 					'error',
