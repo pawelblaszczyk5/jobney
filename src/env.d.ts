b7e5919a9e95e7e53beb4e37681136caabc8a758
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2023-01-22 00:03:15 +0100

    Fix parameter

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 
 declare module 'solid-start-vercel' {
-	function adapter(options: { edge?: boolean; ssr?: boolean }): import('solid-start/vite').Adapter;
+	function adapter(options: { edge?: boolean; prerender?: boolean }): import('solid-start/vite').Adapter;
 
 	export = adapter;
 }
